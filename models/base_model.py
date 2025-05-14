5ab83f327e6c409ddd422b683c0bbf94b2a92ddc
Kawa73jobKotarou <kawa73job@gmail.com>
2025-05-14 13:26:03 +0000

    add LSeSim Loss System

--- models/base_model.py
+++ models/base_model.py
@@ -88,6 +88,9 @@ class BaseModel(ABC):
             self.load_networks(load_suffix)
         self.print_networks(opt.verbose)
 
+    def data_dependent_initialize(self, data):
+        pass
+    
     def eval(self):
         """Make models eval mode during test time"""
         for name in self.model_names:
