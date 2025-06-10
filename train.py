f067ed6b53ac6d112e224ca8adf50c2afc87864b
Kawa73jobKotarou <kawa73job@gmail.com>
2025-06-10 12:34:36 +0000

    dataset no sakusei(hyouji hukume)

--- train.py
+++ train.py
@@ -57,10 +57,14 @@ if __name__ == '__main__':
             model.set_input(data)         # unpack data from dataset and apply preprocessing
             model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
 
+
             if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+                if opt.dataset_mode == "dicom_extention":
+                    visualizer.display_current_results(model.get_current_visuals(), epoch, save_result, mr_bone_flag = data['A_bone_presence'][2], ct_bone_flag = data['B_bone_presence'][2])
+                else:
+                    visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
